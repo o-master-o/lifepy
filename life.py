065954921f93a523797057bc65a4d697d38fd8e2
yoda <kaliamboid@gmail.com>
2018-10-26 00:39:39 +0200

    add default settings

--- life.py
+++ life.py
@@ -5,28 +5,22 @@ import time
 # import pygame
 import configparser
 
-CONFIG_FILE_PATH = r'settings.txt'
+DEFAULTS_CONFIG_FILE_PATH = r'defaults.ini'
+CONFIG_FILE_PATH = r'settings.ini'
 
 
 def start_game():
     config_inst = configparser.RawConfigParser()
-    config_inst.read(CONFIG_FILE_PATH)
+    config_inst.read([DEFAULTS_CONFIG_FILE_PATH, CONFIG_FILE_PATH])
     world_settings = dict(config_inst.items(section='world-config'))
     world_instance = World(world_settings)
 
     world_life_time = world_instance.get_world_life_time()
-    world_age = world_instance.get_age()
     world_life_speed = world_instance.get_life_speed()
-    while world_life_time > world_age:
-        world_age = world_instance.get_age()
+    while world_life_time > world_instance.get_age():
         world_instance.update_world()
         time.sleep(world_life_speed)
 
 
 if __name__ == '__main__':
     start_game()
-
-
-
-# t = Timer(30.0, hello)
-# t.start() # after 30 seconds, "hello, world" will be printed
