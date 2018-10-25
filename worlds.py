01794a5377f442de5c27b0ad5b8ee9de95026855
yoda <kaliamboid@gmail.com>
2018-10-26 00:54:46 +0200

    ad simple pixels

--- worlds.py
+++ worlds.py
@@ -8,8 +8,16 @@ class World(object):
         self.world_settings = world_settings
         self.age = 0
         self.life_speed = 1/float(self.world_settings['world_life_speed'])
-        self.world_size = []
+        self.world_map = self.world_settings['world_size'].split(',')
         self.world_life_time = int(self.world_settings['world_life_time'])
+        self.world_population = int(self.world_settings['world_population'])
+        self.pixel_life_time = int(self.world_settings['pixel_life_time'])
+        self. simplelist = []
+        for count in range(self.world_population):
+            x = Pixel()
+            x.attr = count
+            simplelist.append(x)
+
         print(world_settings)
 
     def get_age(self):
@@ -26,3 +34,15 @@ class World(object):
     def update_world(self):
         log('world_age: ', self.age)
         self.age = self.age + 1
+        __update_pixels()
+
+    def __update_pixels(self):
+        for pixel in world_population:
+            pass
+
+
+class Pixel(object):
+
+    def __init__(self, pixel_life_time):
+        self.pixel_life_time = pixel_life_time
+        self.age = 0
