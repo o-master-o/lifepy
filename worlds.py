01d79794a90568838657f86e90a3f7035193a251
kasian <mikolaj.kasian@nokia.com>
2018-11-07 17:29:14 +0100

    implement __pixel_randomizer in World class

--- worlds.py
+++ worlds.py
@@ -1,4 +1,5 @@
 from tools import *
+import random
 
 
 class World(object):
@@ -7,6 +8,7 @@ class World(object):
         self.age = 0
         self.__set_attributes(world_config)
         self.__create_map()
+        self.__pixel_randomizer()
 
     def __set_attributes(self, dict_config):
         attributes_dict = {}
@@ -16,6 +18,20 @@ class World(object):
 
     def __create_map(self):
         self.map = [[0 for _ in range(self.world_size[0])] for _ in range(self.world_size[1])]
+
+    def __occupy_world(self, occupy_method):
+        self.__pixel_randomizer()
+
+    def __pixel_randomizer(self):
+        print('--------------------')
+        pp.pprint(self.map)
+        for _ in range(self.world_population):
+            h = random.randrange(self.world_size[0]-1)
+            v = random.randrange(self.world_size[1]-1)
+            print(h, v)
+            self.map[h][v] = 1
+        print('--------------------')
+        pp.pprint(self.map)
 
     def get_age(self):
         return self.age
@@ -31,5 +47,3 @@ class World(object):
     def update_world(self):
         log('world_age: ', self.age)
         self.age = self.age + 1
-
-    # def pixel_randomizer(self):
