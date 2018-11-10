97f61296c55296271ae9f565b99acc25b5daaebf
yoda <kaliamboid@gmail.com>
2018-11-10 22:59:30 +0100

    replace configparser by JSONConfigParser fix settings json files

--- life.py
+++ life.py
@@ -2,26 +2,31 @@ from render import *
 from worlds import *
 from tools import *
 import time
+
 # import pygame
 
 
-DEFAULTS_CONFIG_FILE_PATH = r'{}'.format(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'defaults.ini'))
-CONFIG_FILE_PATH = r'{}'.format(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'settings.ini'))
+DEFAULTS_CONFIG_FILE_PATH = r'{}'.format(os.path.join(os.path.dirname(
+    os.path.abspath(__file__)),
+    'defaults.json'))
+CONFIG_FILE_PATH = r'{}'.format(os.path.join(os.path.dirname(
+    os.path.abspath(__file__)),
+    'settings.json'))
 
 
 def start_game():
-    config_inst = AdvancedConfigParser()
-    config_inst.read([DEFAULTS_CONFIG_FILE_PATH, CONFIG_FILE_PATH])
-    world_config = config_inst.filter_dict(['world-config', 'pixel-config'])
+    config_inst = JSONConfigParser()
+    config_inst.read(DEFAULTS_CONFIG_FILE_PATH, CONFIG_FILE_PATH)
+    world_config = config_inst.filter_dict(['world_config'])
 
     world_instance = World(world_config)
     pp.pprint(world_instance.__dict__)
-
-    world_life_time = world_instance.get_world_life_time()
-    world_life_speed = world_instance.get_life_speed()
-    while world_life_time >= world_instance.get_age():
-        world_instance.update_world()
-        time.sleep(world_life_speed)
+    #
+    # world_life_time = world_instance.get_world_life_time()
+    # world_life_speed = world_instance.get_life_speed()
+    # while world_life_time >= world_instance.get_age():
+    #     world_instance.update_world()
+    #     time.sleep(world_life_speed)
 
 
 if __name__ == '__main__':
--- tools.py
+++ tools.py
@@ -1,67 +1,56 @@
-import configparser
 import pprint as pp
+import json
 
 
-class AdvancedConfigParser(configparser.ConfigParser):
+class JSONConfigParser(object):
 
     def __init__(self):
-        super().__init__()
-        self.dict_config = {}
-        self.filtered_dict_config = {}
+        self.configuration = {}
 
-    def filter_dict(self, filter_list=None, auto_type=True):
-        if not self.dict_config:
-            self.get_dict(auto_type)
+    def read(self, *config_files):
+        for file_name in config_files:
+            if isinstance(file_name, str):
+                try:
+                    with open(file_name) as json_data_file:
+                        self.configuration.update(json.load(json_data_file))
+                except OSError:
+                    continue
+
+    def filter_dict(self, filter_list=None):
         if filter_list:
             filter_list = self.force_to_list(filter_list)
-            self.filtered_dict_config.update({k: v for k, v in self.dict_config.items() if k in filter_list})
-        return self.filtered_dict_config
-
-    def get_dict(self, auto_type=True):
-        clear_dict_config = self.__ordered_dict_to_dict()
-        if auto_type:
-            self.dict_config.update(self.__dict_config_auto_type(clear_dict_config))
-        else:
-            self.dict_config.update(clear_dict_config)
-        return self.dict_config
-
-    def __ordered_dict_to_dict(self):
-        dict_config = dict(self._sections)
-        return {k: dict(self._defaults, **dict_config[k]) for k in dict_config}
-
-    def __dict_config_auto_type(self, data):
-        return {section: self.__sub_dict_auto_type(sub_dict) for section, sub_dict in data.items()}
-
-    def __sub_dict_auto_type(self, dict_config):
-        return {k: self.__auto_type(v) for k, v in dict_config.items()}
-
-    def __auto_type(self, untyped_str):
-        for fn in (self.__boolify, self.__listify, int, float):
-            try:
-                typed_str = fn(untyped_str)
-                return typed_str
-            except ValueError:
-                pass
-        return untyped_str
-
-    def __boolify(self, untyped):
-        if untyped == 'True':
-            return True
-        if untyped == 'False':
-            return False
-        raise ValueError("It is not bool Type")
-
-    def __listify(self, untyped):
-        if untyped[0] == '[' and untyped[-1] == ']':
-            listified = untyped[1: -1].split(',')
-            for i, item in enumerate(listified):
-                listified[i] = self.__auto_type(item)
-            return listified
-        raise ValueError("It is not list")
+        return {k: v for k, v in self.configuration.items() if k in filter_list}
 
     @staticmethod
     def force_to_list(parameter):
         return [parameter] if not isinstance(parameter, list) else parameter
+
+
+def auto_type(self, untyped_str):
+    for fn in (self._boolify, self._listify, int, float):
+        try:
+            typed_str = fn(untyped_str)
+            return typed_str
+        except ValueError:
+            pass
+    return untyped_str
+
+
+def _boolify(self, untyped):
+    if untyped == 'True':
+        return True
+    if untyped == 'False':
+        return False
+    raise ValueError("It is not bool Type")
+
+
+def _listify(self, untyped):
+    if untyped[0] == '[' and untyped[-1] == ']':
+        listified = untyped[1: -1].split(',')
+        for i, item in enumerate(listified):
+            listified[i] = self.auto_type(item)
+        return listified
+    raise ValueError("It is not list")
 
 
 def log(comment, msg):
